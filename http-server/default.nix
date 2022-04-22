5213d7da95f6491a026c19ddec3ae30d2d188622
Stephane TEYSSIER <stephane.teyssier@wescale.fr>
2022-04-22 09:57:58 +0200

    Add cachix demo

--- http-server/default.nix
+++ http-server/default.nix
@@ -8,10 +8,10 @@ pkgs.buildGoModule rec {
     owner = "romainDavaze";
     repo = "go-http-server";
     rev = "v${version}";
-    sha256 = "";
+    sha256 = "sha256:1xxj8pvpjz0xlssbf09x1nppsy9rrrzxf2ynlxbjl30ljac8mmr5";
   };
 
-  vendorSha256 = "";
+  vendorSha256 = "sha256:1x1zw8j930midm77cjdr7bz6553lsdj83mvwslgk0501kg8z6w0x";
 
   proxyVendor = true;
 }
